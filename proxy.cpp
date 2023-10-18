4c4085bae6f57d6fd186fb09609e4518d6eca0dd
BartuO <bartuokan1@gmail.com>
2023-10-18 01:13:10 -0600

    add Server-side implementation
    Server-side is added as well. Now the proxy can relay information between client and server over HTTP protocols

--- proxy.cpp
+++ proxy.cpp
@@ -1,62 +1,274 @@
-#include <iostream>
-#include <string>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
 #include <unistd.h>
 
-#define PORT 8080
+#include <arpa/inet.h>
+
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#include <netdb.h>
+
+#include <iostream>
+
+#define CLIENT_PORT 8080
+
+#define SERVER_PORT 80
+
 #define BUFFER_SIZE 4500
+
+
 
 using namespace std;
 
+
+
 int main() {
-    int server_fd, new_socket;
+
+    int server_fd, client_socket;
+
     struct sockaddr_in address;
+
     int addrlen = sizeof(address);
 
+
+
     // Creating socket
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+
         perror("Socket failed");
+
         exit(EXIT_FAILURE);
+
     }
+
+
 
     address.sin_family = AF_INET;
+
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+
+    address.sin_port = htons(CLIENT_PORT);
+
+
 
     // Binding socket to the address
+
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+
         perror("Bind failed");
+
         exit(EXIT_FAILURE);
+
     }
+
+
 
     // Listen for connections
+
     if (listen(server_fd, 3) < 0) {
+
         perror("Listen failed");
+
         exit(EXIT_FAILURE);
+
     }
 
-    cout << "Server started at port " << PORT << ". Waiting for connections..." << endl;
+
+
+    cout << "Server started at port " << CLIENT_PORT << ". Waiting for connections..." << endl;
+
+
+
+    
+
+
 
     while(true) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+
+        //Receving the request from the client
+
+        if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+
             perror("Accept failed");
+
             exit(EXIT_FAILURE);
+
         }
+
+
 
         char buffer[BUFFER_SIZE];
 
-        int bytes_received = recv(new_socket, buffer, BUFFER_SIZE, 0);
+
+
+        int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+
         
+
+
 
         cout << buffer << endl;
 
+
+
+        //Sending the request to the server
+
+
+
+        //Server-side socket creation
+
+        int server_socket = 0;
+
+        server_socket = socket(AF_INET, SOCK_STREAM, 0) ;
+
+        if (server_socket == -1){
+
+            cout << "Socket creation failed !" << endl ;
+
+        }
+
+
+
+        // Set server address details
+
+        struct sockaddr_in server_address ;
+
+        server_address.sin_family = AF_INET;
+
+        server_address.sin_port = htons(SERVER_PORT) ;
+
+
+
+
+
+        if (bytes_received > 0) {
+
+
+
+            buffer[bytes_received] = '\0';
+
+
+
+            string request(buffer);
+
+
+
+            size_t host_pos = request.find("Host:");
+
+            if (host_pos != string::npos) {
+
+                size_t end_pos = request.find("\r\n", host_pos);
+
+                string host = request.substr(host_pos + 6, end_pos - host_pos - 6);
+
+                
+
+
+
+                
+
+                
+
         
 
+                hostent * record = gethostbyname(host.c_str());
+
+                in_addr * s_address = (in_addr * )record->h_addr;
+
+                server_address.sin_addr.s_addr = inet_addr(inet_ntoa(* s_address));
+
+                int status = connect(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+
+                if (status == -1){
+
+                    cout << "connection failed!" << endl;
+
+                    return -1 ;
+
+                }
 
 
-        close(new_socket);
+
+                send(server_socket, request.c_str(), request.length(), 0);
+
+
+
+                char buffer2[4096] = {0};
+
+                int bytesReceived2 = 0;
+
+                while ((bytesReceived2 = recv(server_socket, buffer2, 4096, 0)) > 0) {
+
+                    //std::cout.write(buffer2, bytesReceived2);
+
+                }
+
+
+
+
+
+                string response(buffer2);
+
+
+
+                cout << endl;
+
+                cout <<response << endl;
+
+                
+
+                if(send(client_socket, buffer2, sizeof(buffer2), 0) == -1){
+
+                    perror("Send back to client failed!");
+
+                }
+
+                
+
+                
+
+
+
+
+
+                
+
+
+
+
+
+            }
+
+
+
+            
+
+        }
+
+        
+
+        close(server_socket);
+
+
+
     }
 
+    
+
+    close(client_socket);
+
+    
+
+
+
     return 0;
+
 }
+
